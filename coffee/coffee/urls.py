893ba72543102a77baeb8af6f43ac8fe7154a711
DANIEL1475 <150050739+DANIEL1475@users.noreply.github.com>
2024-06-13 23:17:05 +0330

    Update urls.py

--- coffee/coffee/urls.py
+++ coffee/coffee/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for coffee project.
+URL configuration for coffee_shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('shop.urls')),
+    
+
 ]
