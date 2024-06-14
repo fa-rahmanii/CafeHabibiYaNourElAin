37c5c90f45a70bf4d417395d653f8adf10cb537f
DANIEL1475 <150050739+DANIEL1475@users.noreply.github.com>
2024-06-14 23:11:13 +0330

    Update admin.py

--- coffee/shop/admin.py
+++ coffee/shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Storage, Order, OrderProduct, Category, SliderItem
 
 # Register your models here.
+
+admin.site.register(Storage)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Category)
+admin.site.register(SliderItem)
