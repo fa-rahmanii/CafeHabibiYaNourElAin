8706d8f58aebdaa1ae968e2edb3d8db57e1cae01
fa-rahmanii <frahmanishamsi@gmail.com>
2024-06-23 23:47:19 +0330

    Add files via upload

---
+++ coffee/shop/urls (3).py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('products/', views.product_page, name='products'),
+    path('cart/', views.cart, name='cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:order_product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('purchase-history/', views.purchase_history, name='purchase_history'),
+    path('get-categories/', views.get_categories, name='get_categories'),
+    path('proceed-to-checkout/', views.proceed_to_checkout, name='proceed_to_checkout'),
+    path('sales-chart/', views.sales_chart_view, name='sales_chart'),
+]
