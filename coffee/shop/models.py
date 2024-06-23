8c9d9fad5644845d555c6e26f115a1c33978a41f
DANIEL1475 <150050739+DANIEL1475@users.noreply.github.com>
2024-06-23 23:49:50 +0330

    Update models.py

--- coffee/shop/models.py
+++ coffee/shop/models.py
@@ -18,7 +18,7 @@ class Storage(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.PositiveIntegerField()
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
