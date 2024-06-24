61af2f4aad1b615792c0a75f3cf6b7ceda9fa67f
fa-rahmanii <frahmanishamsi@gmail.com>
2024-06-24 14:53:38 +0330

    Update wsgi.py

--- coffee/coffee/wsgi.py
+++ coffee/coffee/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coffee_shop project.
+WSGI config for coffee project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee.settings')
 
 application = get_wsgi_application()
